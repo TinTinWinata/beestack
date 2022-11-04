a970e125c19924910c282ae9b3665cfdc76347b2
TinTinWinata <tintin6892@gmail.com>
2022-11-04 20:39:19 +0700

    initial android

---
+++ application/app/src/main/java/edu/bluejack22_1/beestack/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.bluejack22_1.beestack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
